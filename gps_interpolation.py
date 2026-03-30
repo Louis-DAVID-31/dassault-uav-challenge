ae9e7eb05095ab0fb90a2ee2c4c8cfb93f7db9e9
Louis David <louis.david162004@gmail.com>
2026-03-30 18:51:31 +0200

    add: gps_interpolation

---
+++ gps_interpolation.py
@@ -0,0 +1,6 @@
+def interpolate_gps_location(pixel_x, pixel_y,
+                             camera_matrix, dist_coeffs,
+                             uav_lat, uav_long, uav_alt,
+                             uav_roll, uav_pitch, uav_yaw,
+                             gimbal_pitch, gimbal_yaw) :
+    pass
