0c55607b4ec92ba105c27d2f188d7ba3c4293673
Louis David <louis.david162004@gmail.com>
2026-04-05 20:56:48 +0200

    add: main

---
+++ main.py
@@ -0,0 +1,7 @@
+from config_manager import load_config, ExecutionConfig, Camera, Detection, OutputConfig
+
+# ==========================================
+# 1. CONFIGURATION
+# ==========================================
+
+EXECUTION_CONFIG, CAMERA, DETECTION, OUTPUT_CONFIG = load_config("config.json")
