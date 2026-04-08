787c614271d11134ede55ce2e00fddd2bb9d8ebd
Louis David <louis.david162004@gmail.com>
2026-04-08 09:02:26 +0200

    update: detection add in main

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from core.config_manager import load_config, ExecutionConfig, Camera, Detection, OutputConfig
+from reporting import Log
+from payload import detection
+from datetime import datetime
 
 # ==========================================
 # 1. CONFIGURATION
 # ==========================================
 
+START_TIME = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
 EXECUTION_CONFIG, CAMERA, DETECTION, OUTPUT_CONFIG = load_config("config.json")
+
+LOG = Log(START_TIME, EXECUTION_CONFIG, OUTPUT_CONFIG)
+LOG.global_header()
 
 # ==========================================
 # 2. DETECTION
 # ==========================================
+
+lat, long = detection(EXECUTION_CONFIG, CAMERA, DETECTION, OUTPUT_CONFIG, LOG)
