a5eddc34bf123d451883c6bccfc4fb0f85d6794f
Louis David <louis.david162004@gmail.com>
2026-04-09 17:26:37 +0200

    delete: unused file

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -1,12 +1,8 @@
 from .log import Log, Detection_Event
-from .mavlink_sender import MavlinkSender
 from .terminal_display import TerminalDisplay
-from .mission_reporter import MissionReporter
 
 __all__ = [
     "Log",
     "Detection_Event",         
-    "MavlinkSender", 
-    "TerminalDisplay",
-    "MissionReporter"
+    "TerminalDisplay"
 ]
